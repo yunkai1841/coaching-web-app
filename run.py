b3d76b7dc40034eac4d8591ca6bd9da909cc9fea
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2022-09-23 17:21:28 +0900

    remove test server

--- run.py
+++
@@ -1,6 +0,0 @@
-import logging
-import os
-logging.basicConfig(level=logging.DEBUG)
-from app import server
-if __name__ == '__main__':
-    server.app.run(host='localhost', port=int(os.environ.get('PORT', 9000)))
