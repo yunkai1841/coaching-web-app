15bddebd0c8aa64c60936bc7c76178eb27fd6bbd
TaichiN27 <ragu.cap@icloud.com>
2022-09-23 15:46:36 +0900

    second commit

--- react_app/src/components/Home.js
+++ react_app/src/components/Home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <h1>ホーム</h1>
+      <h1 className="bg-primary">ホーム</h1>
       <div>
         新規登録は<Link to={`/register/`}>こちら</Link>
       </div>
--- react_app/src/components/Login.js
+++ react_app/src/components/Login.js
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <>
+      <div className="container">
       <h1>ログインページ</h1>
-      <div>
-        新規登録は<Link to={`/register/`}>こちら</Link>
+      <div　className="my-5">
+        <form>
+          ID<input/>
+          PASSWORD<input/>
+        </form>
+        <div>
+          新規登録は<Link to={`/register/`}>こちら</Link>
+        </div>
       </div>
-      <div>
-        <Link to={`/`}>ホームに戻る</Link>
       </div>
     </>
   );
