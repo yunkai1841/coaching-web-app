34a2b2f7d2b9b56e78a3a000298877c5a825d5e7
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2022-09-24 11:26:57 +0900

    add upload api

--- app/server.py
+++ app/server.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from app.apis import hello, articles, videos
+from app.apis import hello, articles, videos, upload
 app = Flask(__name__)
 app.register_blueprint(hello.api)
 app.register_blueprint(articles.api)
 app.register_blueprint(videos.api)
+app.register_blueprint(upload.api)
