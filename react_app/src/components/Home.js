b0ff9ca3f4a9de69a57d892a11358e2c8a381cad
TaichiN27 <ragu.cap@icloud.com>
2022-09-24 10:39:19 +0900

    add props

--- react_app/src/components/Home.js
+++ react_app/src/components/Home.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const Home = () => {
+const Home = (props) => {
   return (
     <>
       <h1 className="bg-primary">ホーム</h1>
--- react_app/src/components/Login.js
+++ react_app/src/components/Login.js
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import React, { useState } from 'react'
 
 const Login = () => {
+
 
   return (
     <>
