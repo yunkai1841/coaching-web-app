8dd4b42c0876a0953217e82de58afe1f6435a9c1
melon3478 <f3478melon@gmail.com>
2022-09-24 09:53:04 +0900

    apisに変更

--- app/apis/user2.py
+++ app/apis/user2.py
@@ -1,7 +1,7 @@
 from flask import Blueprint,render_template
 api = Blueprint('user2',
                 __name__,
-                url_prefix='/api/user2')
+                url_prefix='/apis/user2')
 @api.route('')
 def sample_form():
     return render_template('react_app/src/Home.js')
