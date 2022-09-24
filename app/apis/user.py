ccebff01bb18e0603858c5742b88d895712bab9c
melon3478 <82527101+melon3478@users.noreply.github.com>
2022-09-24 11:21:21 +0900

    Update user.py
    ちゃんとしたやつに変更

--- app/apis/user.py
+++ app/apis/user.py
@@ -1,8 +1,20 @@
-from flask import Blueprint,render_template
+from flask import Blueprint, jsonify, request
 from app.models import user
 api = Blueprint('user',
                 __name__,
                 url_prefix='/api/user')
 @api.route('')
-def sample_form():
-    return render_template('react_app/src/components/Home.js')
+def get_all():
+    data = user.get()
+    return jsonify(data), 200
+@api.route('/<id>')
+def get_by_id(id):
+    data = user.get(id)
+    if len(data) > 0:
+        return jsonify(data), 200
+    else:
+        return jsonify({'errors': [{'message': 'NOT FOUND'}]}), 404
+@api.route('', methods=['POST'])
+def post():
+    data = user.post(str(request.form['password']))
+    return jsonify(data), 201
