9347af286d54882b0347da207d6df3b4a2997863
melon3478 <82527101+melon3478@users.noreply.github.com>
2022-09-24 11:19:12 +0900

    Update comment.py
    articlesをcommentに

--- app/apis/comment.py
+++ app/apis/comment.py
@@ -5,18 +5,18 @@ api = Blueprint('comment',
                 url_prefix='/api/comment')
 @api.route('')
 def get_all():
-    data = articles.get()
+    data = comment.get()
     return jsonify(data), 200
 @api.route('/<id>')
 def get_by_id(id):
-    data = articles.get(id)
+    data = comment.get(id)
     if len(data) > 0:
         return jsonify(data), 200
     else:
         return jsonify({'errors': [{'message': 'NOT FOUND'}]}), 404
 @api.route('', methods=['POST'])
 def post():
-    data = articles.post(
+    data = comment.post(
         str(request.form['video_id']),
         str(request.form['comment'])
     )
