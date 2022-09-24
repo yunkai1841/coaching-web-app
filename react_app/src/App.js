b65dc6c96d44b7d49e245748b9648d4c1d7f67a1
Ika215 <summerwave345@gmail.com>
2022-09-24 11:09:28 +0900

    ビデオページの追加

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import Video from "./components/Video";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path={`/register/`} element={<Register />} />
         <Route path={`/login/`} element={<Login />} />
         <Route path={`/post/`} element={<Login />} />
+        <Route path={`/video/`} element={<Video />} />
 
       </Routes>
     </BrowserRouter>
