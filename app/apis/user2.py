875ed989ac2116de8dc71cfcd917f0ba7be720ed
Yunkai <70383706+yunkai1841@users.noreply.github.com>
2022-09-24 10:55:40 +0900

    Delete user2.py

--- app/apis/user2.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint,render_template
-api = Blueprint('user2',
-                __name__,
-                url_prefix='/apis/user2')
-@api.route('')
-def sample_form():
-    return render_template('react_app/src/components/Home.js')
